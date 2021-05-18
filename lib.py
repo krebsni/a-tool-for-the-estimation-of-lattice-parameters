fbf83d728c96c396650a86097f811e6f79ae9e0e
krebsni <st151522@stud.uni-stuttgart.de>
2021-05-18 15:09:22 +0200

    Initial commit.
    Added submodules, example usage and library files.

---
+++ lib.py
@@ -0,0 +1,28 @@
+#!/usr/bin sage
+import os
+import sys
+import traceback
+
+import fire
+
+import sympy
+
+import random
+
+import time
+from datetime import timedelta
+
+import bisect
+
+from collections import deque
+
+import multiprocessing
+
+from collections import OrderedDict
+
+from sage.all import *
+
+sys.path.append(os.path.dirname(__file__) + "/estimate-all-the-lwe-ntru-schemes.github.io")
+from estimator import estimator
+
+print("hello")
