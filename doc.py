73f0b4f5eb8ecc9ac2bf87fac0c92f9aeb0ea817
krebsni <st151522@stud.uni-stuttgart.de>
2021-07-14 16:53:21 +0200

    moved files to lib folder

---
+++ doc.py
@@ -0,0 +1,11 @@
+import fire
+import os
+if __name__ == "__main__":
+    # estimation_example()
+    class Commandline:
+        def doc(self):
+            # requires sphinx (e.g. "pip install sphinx")
+            os.system("sphinx-apidoc -o doc lib usage_example.py") # EXCLUDE usage_example for now as it does not "compile" at all
+            os.system("sphinx-build doc doc/html")
+
+    fire.Fire(Commandline)
